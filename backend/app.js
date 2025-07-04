d97adc33497cc97dc612fceaf619dfabc7fdb848
utkarshBhandari <utkarshX-dev@users.noreply.github.com>
2025-07-04 22:11:30 +0530

    fix cors

--- backend/app.js
+++ backend/app.js
@@ -1,17 +1,23 @@
-require('dotenv').config();
-const mongoose = require('mongoose');
-const express = require('express');
+require("dotenv").config();
+const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
-const userRoutes = require('./routes/userRoute.js');
-const postRoutes = require('./routes/postRoute.js');
-const commentRoutes = require('./routes/commentRoutes.js');
+const userRoutes = require("./routes/userRoute.js");
+const postRoutes = require("./routes/postRoute.js");
+const commentRoutes = require("./routes/commentRoutes.js");
 const utilRoutes = require("./routes/utilRoutes.js");
 
-const path = require('path');
-const cors = require('cors');
+const path = require("path");
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://dtunite.vercel.app", "http://localhost:5173"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true,
+  })
+);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.json());
@@ -23,27 +29,29 @@ app.use("/api/comments", commentRoutes);
 app.use(express.static(path.join(__dirname, "public")));
 
 const port = process.env.PORT || 8080;
-const MONGO_URL= process.env.MONGO_URL;
+const MONGO_URL = process.env.MONGO_URL;
 app.get("/", (req, res) => {
-    res.send("Welcome to the backend of DTU Reddit");
+  res.send("Welcome to the backend of DTU Reddit");
 });
 
 async function connectdb() {
-    try {
-        await mongoose.connect(MONGO_URL);
-        console.log("connected to database");
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    await mongoose.connect(MONGO_URL);
+    console.log("connected to database");
+  } catch (error) {
+    console.log(error);
+  }
 }
 connectdb();
 app.use((req, res) => {
-  res.status(404).render('pageNotFound.ejs');
+  res.status(404).render("pageNotFound.ejs");
 });
 app.use((err, req, res, next) => {
   console.error(err);
-  res.status(err.status || 500).json({ message: err.message || "Something went wrong" });
+  res
+    .status(err.status || 500)
+    .json({ message: err.message || "Something went wrong" });
 });
-app.listen(port, ()=>{
-    console.log("app is listening to port 8080");
-})
+app.listen(port, () => {
+  console.log(`app is listening to port ${port}`);
+});
