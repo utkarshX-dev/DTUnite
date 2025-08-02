1fa65aeeb65a3d6234b438a19c38b26951e9c0d0
utkarshBhandari <utkarshX-dev@users.noreply.github.com>
2025-08-03 01:05:20 +0530

    C:/Program Files/Git/

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -229,7 +229,7 @@ function Navbar() {
                   fontFamily: "Arial, sans-serif",
                 }}
               >
-                DTUnite
+                Deepak Sharma
               </Typography>
             </Link>
           </Box>
